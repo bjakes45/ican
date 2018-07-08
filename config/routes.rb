59afc8bcacd1de6282fa0e190f3a885ca560e673
bjakes45 <bjakes45@gmail.com>
2018-07-08 10:09:26 -0700

    Simplify signup url

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
