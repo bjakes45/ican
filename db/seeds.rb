8758c7d1999c15695c5a6f1f0ec2c404e7bfb98f
bjakes45 <bjakes45@gmail.com>
2018-07-09 21:26:06 -0700

    Add faker gem. Create seeds for User, Council and Membership models for devenv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def seed_users
+  user_id = 0
+  10.times do 
+    User.create(
+      name: "test#{user_id}",
+      email: "test#{user_id}@test.com",
+      password: '123456',
+      password_confirmation: '123456'
+    )
+    user_id = user_id + 1
+  end
+end
+
+def seed_councils
+
+    5.times do
+      Council.create(
+        title: Faker::Lorem.sentences[0], 
+        description: Faker::Lorem.sentences[0], 
+        user_id: rand(1..9), 
+      )
+    end
+
+end
+
+def seed_memberships
+ 	users = User.all
+
+  	users.each do |user|
+      	Membership.create(
+	        user_id: user.id, 
+	        council_id: rand(1..4),
+          active: true
+	    )
+    end
+
+end
+
+seed_users
+seed_councils
+seed_memberships
